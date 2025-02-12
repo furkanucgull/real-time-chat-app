f098ec8c9a49bcba80e1e7856faacee2b2d7ec62
furkanucgull <furkanucgull@gmail.com>
2025-02-12 14:58:03 +0300

    serverside

---
+++ app/pages/api/socket.js
@@ -0,0 +1,17 @@
+// pages/api/socket.js
+import { Server } from 'socket.io';
+
+export default function handler(req, res) {
+  if (!res.socket.server.io) {
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+
+    io.on('connection', (socket) => {
+      console.log('A user connected');
+      socket.on('message', (msg) => {
+        io.emit('message', msg);
+      });
+    });
+  }
+  res.end();
+}
--- lib/socketClient.ts
+++ lib/socketClient.ts
@@ -1,6 +1,3 @@
 'use client';
 import { io } from 'socket.io-client';
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'https://realtimechat-application.vercel.app/', {
-  path: '/api/socket',
-});
-export { socket };
+export const socket = io('https://real-time-chat-app-plum-seven.vercel.app/');
